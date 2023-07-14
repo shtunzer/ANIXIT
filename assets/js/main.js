40256b37fdc02df7dfbfbf6886b39ed49774c514
Misha121212 <99733113+Misha121212@users.noreply.github.com>
2023-07-14 12:22:52 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,36 @@ document.getElementById('login-open').addEventListener('click', function(){
     document.getElementById('login-popup').classList.add('--show');
     document.getElementById('login-bg').classList.add('--show');
 });
+
 document.getElementById('login-hide').addEventListener('click', function(){
-    document.getElementById('login-popup').classList.remove('--show');
+    var loginPopup = document.getElementById('login-popup');
+    loginPopup.classList.add('--closing');
     document.getElementById('login-bg').classList.remove('--show');
+
+    
+    setTimeout(function() {
+        loginPopup.classList.remove('--show', '--closing');
+    }, 500); 
+    centerLoginPopup();
 });
 
 document.getElementById('reg-open').addEventListener('click', function(){
     document.getElementById('reg-popup').classList.add('--show');
     document.getElementById('login-bg').classList.add('--show');
 });
+
 document.getElementById('reg-hide').addEventListener('click', function(){
-    document.getElementById('reg-popup').classList.remove('--show');
+    var regPopup = document.getElementById('reg-popup');
+    regPopup.classList.add('--closing');
     document.getElementById('login-bg').classList.remove('--show');
+
+    
+    setTimeout(function() {
+        regPopup.classList.remove('--show', '--closing');
+    }, 500); 
+    centerLoginPopup();
 });
+
+
+window.addEventListener('load', centerLoginPopup);
+window.addEventListener('resize', centerLoginPopup);
